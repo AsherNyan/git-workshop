99cfc827d9fe4641c4fc75b74eb40b5f84e3e21f
AsherNyan <dar.demina08@gmail.com>
2019-10-05 17:40:08 +0500

    for merging with the best headgirl in the world

--- musicbox/musicbox.ino
+++ musicbox/musicbox.ino
@@ -12,22 +12,14 @@ Button buttonOff(PIN_BUTTON_OFF);
 Button buttonSpeed(PIN_BUTTON_SPEED);
 Buzzer buzzer(PIN_BUZZER);
 
-int notes[] = {NOTE_FS5, NOTE_CS6, NOTE_GS5, NOTE_SILENCE, NOTE_A5, NOTE_A5, NOTE_FS5, NOTE_CS6, NOTE_GS5, NOTE_A5, NOTE_B5, NOTE_A5, NOTE_FS5, NOTE_GS5, NOTE_E5, NOTE_D5, NOTE_E5, NOTE_FS5, NOTE_D5, NOTE_CS5, NOTE_D5, NOTE_E5};
-double durations[] = {6, 6, 12, 2, 4, 4, 6, 6, 9, 5, 5, 6, 6, 12, 6, 6, 6, 12, 6, 6, 6, 6};
-int melodyLength = 22;
+int notes[] = {NOTE_FS5, NOTE_CS6, NOTE_GS5, NOTE_A5, NOTE_A5, NOTE_FS5, NOTE_CS6, NOTE_GS5, NOTE_A5, NOTE_B5, NOTE_A5, NOTE_FS5, NOTE_GS5, NOTE_E5, NOTE_D5, NOTE_E5, NOTE_FS5, NOTE_D5, NOTE_CS5, NOTE_D5, NOTE_E5, NOTE_FS5, NOTE_GS5, NOTE_A5, NOTE_B5, NOTE_A5, NOTE_FS5, NOTE_CS6, NOTE_GS5, NOTE_A5, NOTE_A5, NOTE_FS5, NOTE_CS6, NOTE_GS5, NOTE_A5, NOTE_B5, NOTE_A5, NOTE_FS5, NOTE_GS5, NOTE_E5, NOTE_D5, NOTE_E5, NOTE_FS5, NOTE_D5, NOTE_CS5, NOTE_D5, NOTE_B5, NOTE_A5, NOTE_GS5};
+double durations[] = {6, 6, 12, 4, 4, 6, 6, 9, 5, 5, 6, 6, 12, 6, 6, 6, 12, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6};
+int melodyLength = 50;
 
 unsigned long speeds[] = {25, 50, 100, 200, 400, 800};
 int currentSpeed = 2;
 int speedsLength = 6;
 
-// maybe somewhere in the future we will have one more button...
- #define PIN_BUTTON_MEL_TWO 4
- Button buttonMelodyTwo(PIN_BUTTON_MEL_TWO);
-
-// and the second melody
- int notes2[] = {NOTE_C4, NOTE_SILENCE, NOTE_G4, NOTE_SILENCE};
- double durations2[] = {4, 1, 4, 1};
- int melodyLength2 = 4;
 
 void setup()
 {
